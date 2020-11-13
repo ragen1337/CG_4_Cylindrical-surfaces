8fc12362171e1f07b55993f3fe286ed9e336a763
ragen1337 <rabgospodanashego@gmail.com>
2020-11-13 19:22:55 +0300

    Добавьте файлы проекта.

---
+++ CG_4_Cylindrical surfaces/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace CG_4_Cylindrical_surfaces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1033, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "a:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1068, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1087, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "b:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1012, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Draw Elliptical cylinder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1012, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1012, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1068, 144);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(53, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1033, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "a:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1087, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "b:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1012, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 47);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Draw  Hyperbolic cylinder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1039, 247);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(53, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1012, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 47);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Draw Parabolic cylinder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1059, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "p:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(999, 576);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 586);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CG_4_Cylindrical surfaces/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CG_4_Cylindrical_surfaces
+{
+    public partial class Form1 : Form
+    {
+        int x = 100;
+        int z = 100;
+        double[,,] pointsArray;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EllipticalCylinderCoord(out double[,,] pointsArray, int x, int z, double a, double b)
+        {
+            pointsArray = new double[2 * x, 3, 2 * z];
+            for (int i = -x; i < x; i++)
+            {
+                for (int j = -z; j < z; j++)
+                {
+                    pointsArray[i + x, 0, j + z] = i;
+                    pointsArray[i + x, 1, j + z] = (b/a) * Math.Sqrt( a*a - i*i );
+                    if( a * a - i * i == 0)
+                    {
+                        pointsArray[i + x, 1, j + z] = 0.000001;
+                    }
+                    pointsArray[i + x, 2, j + z] = j;
+                }
+            }
+        }
+
+        private void HyperbolicCylinderCoord(out double[,,] pointsArray, int x, int z, double a, double b)
+        {
+            pointsArray = new double[2 * x, 3, 2 * z];
+            for (int i = -x; i < x; i++)
+            {
+                for (int j = -z; j < z; j++)
+                {
+                    pointsArray[i + x, 0, j + z] = i;
+                    pointsArray[i + x, 1, j + z] = (b / a) * Math.Sqrt( i * i - a*a );
+                    pointsArray[i + x, 2, j + z] = j;
+                }
+            }
+        }
+
+        private void ParabolicCylinderCoord(out double[,,] pointsArray, int x, int z, double p)
+        {
+            pointsArray = new double[2 * x, 3, 2 * z];
+            for (int i = -x; i < x; i++)
+            {
+                for (int j = -z; j < z; j++)
+                {
+                    pointsArray[i + x, 0, j + z] = i;
+                    pointsArray[i + x, 1, j + z] = Math.Sqrt(2 * p * i);
+                    pointsArray[i + x, 2, j + z] = j;
+                }
+            }
+        }
+
+        private void DrawCylinderSurface(double[,,] pointsArray, int x, int z, Graphics g)
+        {
+            double x0, y0;
+            double picture = 50;
+            PointF point1;
+            PointF point2;
+            Pen blackPen_1 = new Pen(Color.Black, 2);
+
+            for (int i = -x; i < x; i++)
+            {
+                for (int j = -z; j < z; j++)
+                {
+                    x0 = ((pointsArray[i + x, 0, j + z] * picture) / pointsArray[i + x, 2, j + z]) + pictureBox1.Width / 2;
+                    y0 = pictureBox1.Height / 2 - (pointsArray[i + x, 1, j + z] * picture) / pointsArray[i + x, 2, j + z];
+
+                    if (double.IsNaN(y0))
+                    {
+                        continue;
+                    }
+
+                    point1 = new PointF((float)x0, (float)y0);
+                    point2 = new PointF((float)x0 + 1, (float)y0 + 1);
+
+                    g.DrawLine(blackPen_1, point1, point2);
+
+                    y0 = pictureBox1.Height / 2 - (-pointsArray[i + x, 1, j + z] * picture) / pointsArray[i + x, 2, j + z];
+
+                    point1 = new PointF((float)x0, (float)y0);
+                    point2 = new PointF((float)x0 + 1, (float)y0 + 1);
+
+                    g.DrawLine(blackPen_1, point1, point2);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Brush brush = new SolidBrush(Color.WhiteSmoke);
+            g.FillRectangle(brush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            double a;
+            double b;
+            bool a_res = Double.TryParse(textBox3.Text, out a);
+            bool b_res = Double.TryParse(textBox2.Text, out b);
+
+            if (a < 0 || !a_res)
+                a = 100;
+
+            if (b < 0 || !b_res)
+                b = 100;
+
+            EllipticalCylinderCoord(out pointsArray, x, z, a, b);
+
+            DrawCylinderSurface(pointsArray, x, z, g);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Brush brush = new SolidBrush(Color.WhiteSmoke);
+            g.FillRectangle(brush, 0, 0, pictureBox1.Width , pictureBox1.Height);
+
+            double a;
+            double b;
+            bool a_res = Double.TryParse(textBox1.Text, out a);
+            bool b_res = Double.TryParse(textBox4.Text, out b);
+
+            if (a < 0 || !a_res)
+                a = 100;
+
+            if (b < 0 || !b_res)
+                b = 100;
+
+            HyperbolicCylinderCoord(out pointsArray, x, z, a, b);
+
+            DrawCylinderSurface(pointsArray, x, z, g);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Brush brush = new SolidBrush(Color.WhiteSmoke);
+            g.FillRectangle(brush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            double p;
+            bool p_res = Double.TryParse(textBox5.Text, out p);
+
+            if (p < 0 || !p_res)
+                p = 100;
+
+            ParabolicCylinderCoord(out pointsArray, x, z, p);
+         
+            DrawCylinderSurface(pointsArray, x, z, g);
+
+            g.FillRectangle(brush, pictureBox1.Width/2, 0, pictureBox1.Width/2, pictureBox1.Height);
+        }
+    }
+}
